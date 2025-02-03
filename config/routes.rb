a6b8fd54b7384066451a99b4ff9622b375097d10
Jayps <john.paul.lucaylucay.estanislao@gmail.com>
2025-02-03 09:01:29 +0000

    Friends App CRUD and Basic bootstrap design

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :friends
+  # get 'home/index'
+
+  root 'home#index'
+  get 'home/about'
+
+  get 'friends/:id/destroy', to: 'friends#destroy', as: 'destroy_friend'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
